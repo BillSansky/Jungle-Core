e1157b7b4104b51d646578b685e23f2183d6e5bd
Bill Sansky <besnard.william@gmail.com>
2025-09-24 13:33:30 +0200

    added support for conditions

---
+++ Scripts/Condition/Condition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Jungle.Conditions
+{
+    /// <summary>
+    /// Abstract base class for creating conditions that can be evaluated in the Octoputs system.
+    /// Inherit from this class to create custom conditions that return true/false based on your logic.
+    /// </summary>
+    [Serializable]
+    public abstract class Condition
+    {
+        [SerializeField]
+        private bool negateCondition;
+
+        /// <summary>
+        /// Evaluates whether the condition is currently valid.
+        /// </summary>
+        /// <returns>True if the condition is met, false otherwise.</returns>
+        public bool IsValid()
+        {
+            return negateCondition ? !IsValidImpl() : IsValidImpl();
+        }
+        
+        protected internal abstract bool IsValidImpl();
+    }
+}
---
+++ Scripts/Condition/ExternalCondition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Jungle.Conditions
+{
+    /// <summary>
+    /// A simple boolean condition that can be toggled in the inspector.
+    /// Useful for testing or creating simple on/off conditions.
+    /// </summary>
+    [Serializable]
+    public class ExternalCondition : Condition
+    {
+        [SerializeField] private Object conditionProvider;
+        
+        /// <summary>
+        /// Evaluates the boolean condition.
+        /// </summary>
+        /// <returns>The current value of the boolean condition.</returns>
+        protected internal override bool IsValidImpl()
+        {
+            return ((IBooleanCondition)conditionProvider).Condition;
+        }
+    }
+    
+    public interface IBooleanCondition
+    {
+        bool Condition { get; }
+    }
+
+}
